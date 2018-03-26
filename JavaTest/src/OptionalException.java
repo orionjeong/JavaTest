3b1574be6a043a07c526b01c7a35a75577b14807
orionjeong <djflqjfl0209@gmail.com>
2018-03-26 17:13:01 +0900

    Optional Exception Improving

---
+++ JavaTest/src/OptionalException.java
@@ -0,0 +1,48 @@
+import java.util.Optional;
+
+public class OptionalException {
+
+
+}
+
+class ContInfo{
+    String phone;
+    String adrs;
+
+    public ContInfo(String ph, String ad){
+        phone = ph;
+        adrs = ad;
+    }
+
+    public String getPhone(){
+        return phone;
+    }
+    public String getAdrs(){
+        return adrs;
+    }
+}
+
+class ifElseOptional{
+    public static void main(String[] args){
+        Optional<ContInfo> ci = Optional.of(new ContInfo(null, "Republic of Korea"));
+        String phone = ci.map(s-> s.getPhone()).orElse("전화번호가  없어용");
+        String adrs = ci.map(s-> s.getAdrs()).orElse("주소가 없어용");
+
+//        개선 전 코드
+//
+//        if(ci.phone != null){
+//            phone = ci.getPhone();
+//        }else {
+//            phone = "There is no phone number.";
+//        }
+//
+//        if(ci.adrs != null)
+//            addr = ci.getAdrs();
+//        else
+//            addr = "There is no address.";
+
+        System.out.println(phone);
+        System.out.println(adrs);
+
+    }
+}
